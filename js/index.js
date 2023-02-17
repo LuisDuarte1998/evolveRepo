695704af8f6be4a9c678593f0713d6f86218668b
UXbyAndrew <121202549+UXbyAndrew@users.noreply.github.com>
2023-02-17 17:54:43 -0500

    additions:
    stations added, pop up cards added, bottom cards began

--- js/index.js
+++ js/index.js
@@ -5,3 +5,54 @@ menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 })
+
+
+
+// const showCardButton = document.querySelectorAll('.show-card');
+// const card = document.querySelector('.card');
+
+// showCardButton.addEventListener('click', function() {
+//     var station=this.getAttribute("data-location");
+//   var card= document.querySelector(`.card[data-location=${station}]`);
+//   hidecards()
+//   showcard(card)
+
+// });
+
+function hidecards() {
+    document.querySelectorAll('.card').forEach(function (e){
+        console.log(e)
+        e.style.display = 'none';
+    });
+
+}
+
+function showcard(card) {
+    card.style.display = 'block';
+}
+
+var buttons = document.getElementsByClassName("show-card");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        var station=this.getAttribute("data-location");
+      var card= document.querySelector(`.card[data-location=${station}]`);
+      hidecards()
+      showcard(card)
+      // toggle card //
+      removeactive()
+      addactive(this)
+    })}
+
+    function removeactive() {
+        document.querySelectorAll('.show-card').forEach(function (e){
+            console.log(e)
+            e.querySelector('img').src=e.querySelector('img').src.replace('_active', '')
+            // removes _active //
+        });
+    }
+
+function addactive(e) {
+    e.querySelector('img').src=e.querySelector('img').src.replace('.png', '_active.png')
+    // toggles _active on image //
+}
