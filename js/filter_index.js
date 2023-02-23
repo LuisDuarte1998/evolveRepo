8f31aad373b457700ec49da480838853018cc4b3
LuisDuarte1998 <ljd08041998@gmail.com>
2023-02-23 15:11:02 -0500

    functional accordion
    made an accordion that works but still working on spacing

--- js/filter_index.js
+++ js/filter_index.js
@@ -88,3 +88,26 @@ function toggleNetworkPanel(x){
         panel.style.height = maxH;
     }
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
+$(".arrow").click(function(){
+    $(this).toggleClass("down"); 
+})
